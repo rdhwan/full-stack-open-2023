71a9e9b753d5bf37a77ef980136788203b88b931
rdhwan <fathanridhwan@gmail.com>
2023-05-11 08:48:59 +0700

    [fix]: separating weather services

--- part2/countries/src/services/CountriesServices.js
+++ part2/countries/src/services/CountriesServices.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const REST_COUNTRIES_BASE_URL = "https://restcountries.com";
-const OPENWEATHER_BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric&q=`;
 
 const getCountries = () =>
   axios
@@ -11,11 +10,4 @@ const getCountries = () =>
       throw error;
     });
 
-const getWeather = (countryName) =>
-  axios
-    .get(OPENWEATHER_BASE_URL + countryName)
-    .then((response) => response.data)
-    .catch((error) => {
-      throw error;
-    });
-export { getCountries, getWeather };
+export { getCountries };
